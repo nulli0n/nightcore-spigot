adba363de19a96e2244265429eeaf6d006011ed8
nulli0n <glitchy.cat@yandex.ru>
2024-12-09 15:30:00 +0500

    v2.7.3

---
+++ src/main/java/su/nightexpress/nightcore/ui/menu/type/NormalMenu.java
@@ -0,0 +1,18 @@
+package su.nightexpress.nightcore.ui.menu.type;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.MenuType;
+import org.jetbrains.annotations.NotNull;
+import su.nightexpress.nightcore.NightPlugin;
+
+@SuppressWarnings("UnstableApiUsage")
+public abstract class NormalMenu<P extends NightPlugin> extends AbstractMenu<P> {
+
+    public NormalMenu(@NotNull P plugin, @NotNull MenuType menuType, @NotNull String title) {
+        super(plugin, menuType, title);
+    }
+
+    public boolean open(@NotNull Player player) {
+        return this.open(player, viewer -> {});
+    }
+}
